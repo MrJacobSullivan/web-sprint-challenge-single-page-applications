7ec533a36a87648cd5f86fb0987bbba63ee113f6
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-15 12:25:50 -0500

    added react router dom and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { Route } from 'react-router-dom'
 import Layout from './components/Layout'
 
+import Home from './pages/Home'
+import Order from './pages/Order'
+import Confirmation from './pages/Confirmation'
+
 const App = () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <Route exact path='/' component={Home} />
+      <Route path='/order' component={Order} />
+      <Route path='/confirmation' component={Confirmation} />
+    </Layout>
+  )
 }
 export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom'
+
 export default function Header() {
   return (
     <header>
       <h1>Lambda Eats</h1>
-      <nav></nav>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/order'>Order</Link>
+      </nav>
     </header>
   )
 }
